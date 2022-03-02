8062569d0cf60e70f61e4375a592da4f9814d2c9
unknown <succip@gmail.com>
2022-03-02 14:28:21 -0800

    prepared maplayerlistitem

--- src/components/LayerList/LayerListTree.jsx
+++ src/components/LayerList/LayerListTree.jsx
@@ -1,11 +1,21 @@
+import LayerListItem from "./LayerListItem";
 import TreeItem from "@mui/lab/TreeItem";
 import TreeView from "@mui/lab/TreeView";
 import FolderIcon from "@mui/icons-material/Folder";
 import FolderOpenIcon from "@mui/icons-material/FolderOpen";
+import { useSelector } from "react-redux";
+import store from "../../store/store";
+
+const getMapLayerByTitle = (mapLayerTitle) => {
+  const { layers } = store.getState();
+  const { mapLayers } = layers;
+  return mapLayers.find((mapLayer) => mapLayer.title === mapLayerTitle);
+};
 
 const getTreeItemsFromData = (treeItems) => {
   return treeItems.map((treeItemData) => {
     let children = undefined;
+    const mapLayer = getMapLayerByTitle(treeItemData.name);
     if (treeItemData.children && treeItemData.children.length > 0) {
       children = getTreeItemsFromData(treeItemData.children);
     }
@@ -14,11 +24,15 @@ const getTreeItemsFromData = (treeItems) => {
 };
 
 const LayerListTree = ({ treeItems }) => {
+  const { mapLayers } = useSelector((state) => state.layers);
+
   return (
     <>
-      <TreeView defaultCollapseIcon={<FolderOpenIcon />} defaultExpandIcon={<FolderIcon />}>
-        {getTreeItemsFromData(treeItems)}
-      </TreeView>
+      {mapLayers && (
+        <TreeView defaultCollapseIcon={<FolderOpenIcon />} defaultExpandIcon={<FolderIcon />}>
+          {getTreeItemsFromData(treeItems)}
+        </TreeView>
+      )}
     </>
   );
 };
