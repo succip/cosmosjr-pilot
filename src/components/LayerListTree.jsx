c79cecf62648c911d90c359e7cd1b19c49c4ba75
Paolo Succi <succi@Paolos-MacBook-Pro.local>
2022-03-02 07:17:09 -0800

    layers

--- src/components/LayerListTree.jsx
+++ src/components/LayerListTree.jsx
@@ -1,5 +1,27 @@
+import TreeItem from "@mui/lab/TreeItem";
+import TreeView from "@mui/lab/TreeView";
+import FolderIcon from "@mui/icons-material/Folder";
+import FolderOpenIcon from "@mui/icons-material/FolderOpen";
 import React from "react";
 
-export const LayerListTree = () => {
-  return <div>LayerListTree</div>;
+const getTreeItemsFromData = (treeItems) => {
+  return treeItems.map((treeItemData) => {
+    let children = undefined;
+    if (treeItemData.children && treeItemData.children.length > 0) {
+      children = getTreeItemsFromData(treeItemData.children);
+    }
+    return <TreeItem nodeId={treeItemData.id} key={treeItemData.id} label={treeItemData.name} children={children} />;
+  });
 };
+
+const LayerListTree = ({ treeItems }) => {
+  return (
+    <>
+      <TreeView defaultCollapseIcon={<FolderOpenIcon />} defaultExpandIcon={<FolderIcon />}>
+        {getTreeItemsFromData(treeItems)}
+      </TreeView>
+    </>
+  );
+};
+
+export default LayerListTree;
--- src/components/MapLayerList.jsx
+++ src/components/MapLayerList.jsx
@@ -1,33 +1,12 @@
 import React from "react";
-import TreeItem from "@mui/lab/TreeItem";
-import TreeView from "@mui/lab/TreeView";
 import LayerStore from "../config/LayerStore";
-import FolderIcon from "@mui/icons-material/Folder";
-import FolderOpenIcon from "@mui/icons-material/FolderOpen";
-
-const getTreeItemsFromData = (treeItems) => {
-  return treeItems.map((treeItemData) => {
-    let children = undefined;
-    if (treeItemData.children && treeItemData.children.length > 0) {
-      children = getTreeItemsFromData(treeItemData.children);
-    }
-    return <TreeItem nodeId={treeItemData.id} key={treeItemData.name} label={treeItemData.name} children={children} />;
-  });
-};
-
-const DataTreeView = ({ treeItems }) => {
-  return (
-    <TreeView defaultCollapseIcon={<FolderOpenIcon />} defaultExpandIcon={<FolderIcon />}>
-      {getTreeItemsFromData(treeItems)}
-    </TreeView>
-  );
-};
+import LayerListTree from "./LayerListTree";
 
 const MapLayerList = () => {
   return (
     <div>
       <p className="h4">Layer List</p>
-      <DataTreeView treeItems={LayerStore} />
+      <LayerListTree treeItems={LayerStore} />
     </div>
   );
 };
