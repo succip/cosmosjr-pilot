85f4ba5c3b9d13f8caf41b9174031e38d199554e
succip <succip@gmail.com>
2024-07-03 15:42:08 -0700

    widget test

--- src/components/OpenDialogTest.jsx
+++ src/components/OpenDialogTest.jsx
@@ -1,43 +1,37 @@
 import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
 import Paper from "@mui/material/Paper";
 import { useState } from "react";
 import Draggable from "react-draggable";
-import { styled } from "@mui/material/styles";
-
-function PaperComponent(props) {
-  return (
-    <Draggable handle="#draggable-dialog-title" cancel={'[class*="MuiDialogContent-root"]'}>
-      <Paper style={{ position: "absolute", zIndex: 100 }}>
-        <div id="modal-header">{/* Header for dragging */}</div>
-      </Paper>
-    </Draggable>
-  );
-}
+import MapLayerList from "./LayerListLegendPanel/MapLayerList";
 
 const OpenDialogTest = () => {
   const [open, setOpen] = useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
+  const handleClick = () => {
+    setOpen(!open);
   };
 
   return (
     <div>
-      <Button variant="contained" onClick={handleClickOpen}>
-        Open Dialog
+      <Button variant="contained" onClick={handleClick}>
+        {open ? "Close Widget" : "Open Widget"}
       </Button>
       <Draggable handle="#draggable-dialog-title" cancel={'[class*="MuiDialogContent-root"]'}>
-        <Paper style={{ position: "absolute", zIndex: 100 }}>
-          <div id="modal-header">{/* Header for dragging */}</div>
+        <Paper
+          style={{
+            position: "absolute",
+            zIndex: 100,
+            maxWidth: "400px",
+            maxHeight: "500px",
+            overflowY: "auto",
+            padding: "16px",
+            visibility: open ? "visible" : "hidden",
+          }}
+        >
+          <div id="draggable-dialog-title" style={{ cursor: "move" }}>
+            WIDGET TITLE
+          </div>
+          <MapLayerList />
         </Paper>
       </Draggable>
     </div>
--- src/config/WidgetConfig.js
+++ src/config/WidgetConfig.js
@@ -1,8 +1,13 @@
 import MapViewModeToggle from "../components/MapViewModeToggle/MapViewModeToggle";
+import OpenDialogTest from "../components/OpenDialogTest";
 
 export const widgets = [
   {
     component: <MapViewModeToggle />,
+    position: "top-right",
+  },
+  {
+    component: <OpenDialogTest />,
     position: "top-right",
   },
 ];
